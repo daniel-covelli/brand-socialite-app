5e016ff9d05f6e1f1d3943f27127e5fdecedc07b
Daniel Covelli <covellidanny@gmail.com>
2020-07-31 12:10:06 -0700

    Finished Event Header Display

--- components/Event/Header/Description/DateTimeModal.js
+++ components/Event/Header/Description/DateTimeModal.js
@@ -1,43 +1,104 @@
-import { Header, Label, Icon, Modal, Divider } from 'semantic-ui-react';
+import {
+  Header,
+  Label,
+  Icon,
+  Modal,
+  Divider,
+  Responsive
+} from 'semantic-ui-react';
 
 const DateTimeModal = ({ props }) => {
-  console.log(props);
   return (
-    <Modal
-      trigger={
-        <Label as='a' size={'large'} style={{ margin: '0 1em 1em 0' }} basic>
-          <Icon name={props.icon} />
-          {props.eventdate}
-        </Label>
-      }
-      centered={false}>
-      <Modal.Header>Event Schedule</Modal.Header>
-      <Modal.Content>
-        <Header sub>Date</Header>
-        {props.date_formatted_long}
+    <>
+      <Responsive minWidth={768}>
+        <Modal
+          trigger={
+            <Label
+              as='a'
+              size={'large'}
+              style={{ margin: '0 1em 1em 0' }}
+              basic>
+              <Icon name={props.icon} />
+              {props.eventdate}
+            </Label>
+          }
+          centered={false}>
+          <Modal.Header>Event Schedule</Modal.Header>
+          <Modal.Content>
+            <Header sub>Date</Header>
+            {props.date_formatted_long}
 
-        <Divider hidden />
+            <Divider hidden />
 
-        <Header sub>Set Up</Header>
-        {props.setup_timespan}
+            <Header sub>
+              <Icon name='wrench' />
+              Set Up
+            </Header>
+            {props.setup_timespan}
 
-        <Divider hidden />
+            <Divider hidden />
 
-        <Header sub>
-          <Icon name='certificate' />
-          Event
-        </Header>
-        <p>{props.event_timespan}</p>
+            <Header sub>
+              <Icon name='certificate' />
+              Event
+            </Header>
+            <p>{props.event_timespan}</p>
 
-        <Divider hidden />
+            <Divider hidden />
 
-        <Header sub>
-          <Icon name='wrench' />
-          Breakdown
-        </Header>
-        <p>{props.breakdown_timespan}</p>
-      </Modal.Content>
-    </Modal>
+            <Header sub>
+              <Icon name='wrench' />
+              Breakdown
+            </Header>
+            <p>{props.breakdown_timespan}</p>
+          </Modal.Content>
+        </Modal>
+      </Responsive>
+      <Responsive maxWidth={768}>
+        <Modal
+          trigger={
+            <Label
+              as='a'
+              size={'small'}
+              style={{ margin: '0 1em 1em 0' }}
+              basic>
+              <Icon name={props.icon} />
+              {props.eventdate}
+            </Label>
+          }
+          centered={false}>
+          <Modal.Header>Event Schedule</Modal.Header>
+          <Modal.Content>
+            <Header sub>Date</Header>
+            {props.date_formatted_long}
+
+            <Divider hidden />
+
+            <Header sub>
+              <Icon name='wrench' />
+              Set Up
+            </Header>
+            {props.setup_timespan}
+
+            <Divider hidden />
+
+            <Header sub>
+              <Icon name='certificate' />
+              Event
+            </Header>
+            <p>{props.event_timespan}</p>
+
+            <Divider hidden />
+
+            <Header sub>
+              <Icon name='wrench' />
+              Breakdown
+            </Header>
+            <p>{props.breakdown_timespan}</p>
+          </Modal.Content>
+        </Modal>
+      </Responsive>
+    </>
   );
 };
 
